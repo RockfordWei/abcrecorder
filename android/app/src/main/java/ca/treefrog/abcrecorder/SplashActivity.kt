21fdbfdd3258d84679b1923d5dce16fa40464215
Rockford Wei <rocky@perfect.org>
2020-06-23 09:14:08 -0400

    implementing the splash activity

---
+++ android/app/src/main/java/ca/treefrog/abcrecorder/SplashActivity.kt
@@ -0,0 +1,19 @@
+package ca.treefrog.abcrecorder
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    private val timeout: Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, timeout)
+    }
+}
